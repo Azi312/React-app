d2cabdae22cdfdac9ebfb20a844b5aa731af71eb
Azi312 <aziz260110@gmail.com>
2023-05-21 15:22:24 +0600

    start dev

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { store } from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+	<BrowserRouter>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</BrowserRouter>
+)
